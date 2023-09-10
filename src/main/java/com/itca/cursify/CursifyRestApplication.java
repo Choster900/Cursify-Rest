38940fae264ba3e42bc104d7ea1271cf1ec29784
SergioCokie <16adonaysergio@gmail.com>
2023-09-10 12:36:33 -0600

    Init project

---
+++ src/main/java/com/itca/cursify/CursifyRestApplication.java
@@ -0,0 +1,13 @@
+package com.itca.cursify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursifyRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursifyRestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/itca/cursify/controller/RoleController.java
@@ -0,0 +1,7 @@
+package com.itca.cursify.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoleController {
+}
